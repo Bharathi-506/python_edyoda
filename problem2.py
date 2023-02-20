3a0f68af2b4d7e149cb9609dcc80d361819bb23e
Bharathi-506 <125906445+Bharathi-506@users.noreply.github.com>
2023-02-20 22:52:09 +0530

    Add files via upload

---
+++ problem2.py
@@ -0,0 +1,9 @@
+#xWrite a Python program that accepts a word from the user and reverse it.
+x = 0 # x = first number
+y = 1 # y = second number
+print(x)
+print(y)
+for l in range(1,9):
+    z = x + y # z= third number
+    print(z)
+    x,y = y,z
