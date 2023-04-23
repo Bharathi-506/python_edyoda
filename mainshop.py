d2fdd2855a6b5cf4755cfa664400bbfa54ca018e
Bharathi-506 <125906445+Bharathi-506@users.noreply.github.com>
2023-04-23 14:53:23 +0530

    Delete mainshop.py

--- mainshop.py
+++
@@ -1,6 +0,0 @@
-import shop
-key = input('Admin --> A or User --> U')
-if key == 'A':
-    username = input("Enter admin username:")
-    password = input("Enter admin password:")
-    shop.login(username,password )
