a3d1c515007f09122c8c9c222cf15fb36dbe6a3f
Bharathi-506 <125906445+Bharathi-506@users.noreply.github.com>
2023-04-23 14:53:33 +0530

    Delete shop.py

--- shop.py
+++
@@ -1,9 +0,0 @@
-import json
-def login (username, password):
-    with open ('admin.json') as fr:
-        data = json.load(fr)
-        if data['Username'] == username and data['Password'] == password :
-            print("Login successful")
-        else:
-            print("please try with admin details")
-       
